61a6d024151938ed0e8331b4e854f82481866dc0
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-07-02 18:22:56 -0300

    app

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from "@react-navigation/native"
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { LogBox } from 'react-native'
+
+LogBox.ignoreAllLogs(true)
+
+import AuthProvider from './src/contexts/auth';
+
+import Routes from './src/routes';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthProvider>
+        <StatusBar hidden />
+        <Routes />
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
