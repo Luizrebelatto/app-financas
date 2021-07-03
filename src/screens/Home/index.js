9c690a430c17572032c4eca3f4652a8522ac7007
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-07-02 22:54:46 -0300

    add List

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { AuthContext } from '../../contexts/auth'
 
 import Header from '../../components/Header';
-import { Background, Container, Name, Balance, Title } from './styles';
+import HistoricList from '../../components/HistoricList';
+import { Background, Container, Name, Balance, Title, List } from './styles';
 
 export default function Home() {
+
+    const [historic, setHistoric] = useState([
+        { key: '1', tipo: 'receita', valor: 1200 },
+        { key: '2', tipo: 'despesa', valor: 350 },
+        { key: '3', tipo: 'receita', valor: 800 },
+        { key: '4', tipo: 'receita', valor: 89.61 },
+        { key: '5', tipo: 'despesa', valor: 89.61 },
+        { key: '6', tipo: 'despesa', valor: 310 }
+    ])
 
     const { user } = useContext(AuthContext);
 
@@ -18,6 +28,13 @@ export default function Home() {
             </Container>
 
             <Title>Últimas Movimentações</Title>
+
+            <List
+                showsVerticalScrollIndicator={false}
+                data={historic}
+                keyExtractor={item => item.key}
+                renderItem={({ item }) => (<HistoricList data={item} />)}
+            />
         </Background>
     );
 }
