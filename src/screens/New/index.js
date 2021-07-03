07f4a58f851fc848fa7e455cc74f31aa5f406af4
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-07-03 11:32:48 -0300

    screen new

--- src/screens/New/index.js
+++ src/screens/New/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import Header from '../../components/Header';
+import Picker from '../../components/Picker';
+
+import { Background, Input, SubmitButton, SubmitText } from './styles';
+
 
 export default function New() {
+    const [valor, setValor] = useState('');
+    const [type, setType] = useState('receita');
+
     return (
-        <View>
-            <Text>New</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <Background>
+                <Header />
+                <SafeAreaView style={{ alignItems: 'center' }}>
+                    <Input
+                        placeholder="Valor desejado"
+                        keyboardType="numeric"
+                        returnKeyType="next"
+                        onSubmitEditing={() => Keyboard.dismiss()}
+                        value={valor}
+                        onChangeText={(text) => setValor(text)}
+                    />
+
+                    <Picker onChange={setType} tipo={type} />
+
+                    <SubmitButton>
+                        <SubmitText>Registrar</SubmitText>
+                    </SubmitButton>
+                </SafeAreaView>
+            </Background>
+        </TouchableWithoutFeedback>
     );
 }
