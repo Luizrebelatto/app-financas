407280eeda74c35061c668c19654f16ceb87947f
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-07-04 22:12:54 -0300

    function deleteItem and parameters

--- src/components/HistoricList/index.js
+++ src/components/HistoricList/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { AntDesign } from '@expo/vector-icons';
-
+import { TouchableWithoutFeedback } from 'react-native';
 import { Container, Type, IconView, TypeText, ValueText } from './styles'
 
-export default function HistoricList({ data }) {
+export default function HistoricList({ data, deleteItem }) {
     return (
-        <Container>
-            <Type>
-                <IconView tipo={data.tipo}>
-                    <AntDesign
-                        name={data.tipo == 'despesa' ? 'arrowdown' : 'arrowup'}
-                        size={20}
-                        color="white"
-                    />
-                    <TypeText>{data.tipo}</TypeText>
-                </IconView>
-            </Type>
+        <TouchableWithoutFeedback onLongPress={() => deleteItem(data)}>
+            <Container>
+                <Type>
+                    <IconView tipo={data.tipo}>
+                        <AntDesign
+                            name={data.tipo == 'despesa' ? 'arrowdown' : 'arrowup'}
+                            size={20}
+                            color="white"
+                        />
+                        <TypeText>{data.tipo}</TypeText>
+                    </IconView>
+                </Type>
 
-            <ValueText>R$ {data.valor}</ValueText>
-        </Container>
+                <ValueText>R$ {data.valor}</ValueText>
+            </Container>
+        </TouchableWithoutFeedback>
     );
 }
