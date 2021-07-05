22e75a38e8179e4d7bcb5bbcdb732a808f0778e3
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-07-04 21:47:29 -0300

    loading verification

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -7,6 +7,7 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [loadingAuth, setLoadingAuth] = useState(false);
 
     useEffect(() => {
         async function loadStorage() {
@@ -23,6 +24,7 @@ function AuthProvider({ children }) {
     }, [])
 
     async function signIn(email, password) {
+        setLoadingAuth(true);
         await firebase.auth().signInWithEmailAndPassword(email, password)
             .then(async (value) => {
                 let uid = value.user.uid;
@@ -36,14 +38,17 @@ function AuthProvider({ children }) {
 
                         setUser(data);
                         storageUser(data);
+                        setLoadingAuth(false);
                     })
             })
             .catch((error) => {
                 alert(error.code);
+                setLoadingAuth(false);
             });
     }
 
     async function signUp(email, password, nome) {
+        setLoadingAuth(true);
         await firebase.auth().createUserWithEmailAndPassword(email, password)
             .then(async (value) => {
                 let uid = value.user.uid;
@@ -59,10 +64,12 @@ function AuthProvider({ children }) {
                         };
                         setUser(data);
                         storageUser(data);
+                        setLoadingAuth(false);
                     })
             })
             .catch((error) => {
                 alert(error.code);
+                setLoadingAuth(false);
             })
     }
 
@@ -79,7 +86,7 @@ function AuthProvider({ children }) {
     }
 
     return (
-        <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, signOut, loading }}>
+        <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, signOut, loading, loadingAuth }}>
             {children}
         </AuthContext.Provider>
     )
