d6aef2033df247b84e2a0f0eed0981f1a2dbac83
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-07-06 13:41:45 -0300

    content drawer and customization

--- src/components/CustomDrawer/index.js
+++ src/components/CustomDrawer/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { View, Text, Image } from 'react-native';
 
 export default function CustomDrawer() {
     return (
-        <View />
+        <DrawerContentScrollView>
+            <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 25 }}>
+                <Image
+                    source={require('../../assets/Logo.png')}
+                    style={{ width: 85, height: 85 }}
+                    resizeMode="contain"
+                />
+                <Text style={{ color: '#fff', fontSize: 18, marginTop: 5 }}>Bem Vindo</Text>
+                <Text style={{ color: '#fff', fontSize: 17, fontWeight: 'bold', paddingBottom: 25 }}>Luiz</Text>
+            </View>
+
+
+        </DrawerContentScrollView>
     );
 }
